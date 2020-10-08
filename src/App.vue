8d1a2d9c921865352d1b151854b44b948fcc43e2
Borja Gete <borjag90dev@gmail.com>
2020-10-08 13:39:15 +0200

    style: Basic landing style #1 #2

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
+  <router-view />
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
+body {
+  background-image: url("https://images.unsplash.com/photo-1562321157-5fd366036de8?ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=80");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+}
+
 #app {
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
 }
 
 #nav {
@@ -25,6 +33,12 @@
     &.router-link-exact-active {
       color: #42b983;
     }
+  }
+}
+
+@media (min-width: 600px) {
+  body {
+    background: green;
   }
 }
 </style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="landing-container"></div>
+  <div class="landing-container">
+    <router-view />
+    <div id="foot"></div>
+    <h1>ClutchTime</h1>
+    <h2>Manager</h2>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
 </script>
 
 <style>
-
+.landing-container {
+  margin: 0px 8px;
+  padding: 30px;
+  background-image: url("https://images.unsplash.com/photo-1504450758481-7338eba7524a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  border-radius: 5%;
+  min-height: 650px;
+  height: 80%;
+}
+#foot {
+}
 </style>
